c58a6afb7302fbfcbebb4ea79117718a35fefa9f
AminJun <mohammad.amin.ghiasi@gmail.com>
2021-01-05 23:18:01 -0500

    Save all images

--- lisa.py
+++ lisa.py
@@ -5,7 +5,7 @@ from torch.utils.data.dataset import Dataset
 import pandas as pd
 import pdb
 import os
-from tqdm import tqdm 
+from tqdm import tqdm
 
 
 class SlowDataset(Dataset):
@@ -16,31 +16,30 @@ class SlowDataset(Dataset):
         image = Image.open(os.path.join(self.data_root, address))
         return torchvision.transforms.functional.resized_crop(image, y1, x1, y2 - y1, x2 - x1, 32)
 
-    def __init__(self):
-        table = pd.read_csv(self.csv_root, delimiter=';')
-        self.to_tensor = torchvision.transforms.ToTensor()
+    def _read_images(self, table: pd.DataFrame) -> (list, list):
         labels = []
-        one_from_each_label = {}
         images = []
-
         for i, row in tqdm(table.iterrows()):
             file, label, x1, y1, x2, y2, *_ = row
             image = self._load_image(file, x1, y1, x2, y2)
             images.append(image)
             labels.append(label)
-            if label not in one_from_each_label.keys():
-                one_from_each_label[label] = i
-                print(len(one_from_each_label))
-            if len(one_from_each_label) == 47:
-                break
+        return images, labels
 
-        for label, i in one_from_each_label.items():
-            image: Image = images[i]
-            image.save(os.path.join('examples', f'{label}.png'))
+    def save_images(self, images: list, labels: list):
+        for i, (image, label) in enumerate(zip(images, labels)):
+            image.save(os.path.join('desktop', f'{i}_{label}'))
+
+    def __init__(self):
+        table = pd.read_csv(self.csv_root, delimiter=';')
+        self.to_tensor = torchvision.transforms.ToTensor()
+        self.labels, self.images = self._read_images(table)
+        self.save_images(self.images, self.labels)
+        pdb.set_trace()
 
 
 def main():
-    dataset = SlowDataset()
+    SlowDataset()
 
 
 if __name__ == '__main__':
